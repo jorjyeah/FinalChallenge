0fb186e2e9255db5cf6220e27cc5a3c5e82ae6dc
George Joseph Kristian <jorji.gjk@gmail.com>
2019-11-11 18:39:55 +0700

    Initial Commit

---
+++ FinalChallengeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinalChallengeApp
+//
+//  Created by George Joseph Kristian on 11/11/19.
+//  Copyright © 2019 George Joseph Kristian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
