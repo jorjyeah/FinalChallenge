adbc9a00641489e3d2b732f5b8c8c81ae640c81d
Bianka <ni.aristania@binus.ac.id>
2019-11-21 19:35:07 +0700

    add report page

---
+++ FinalChallengeApp/View/Cell/ActivityTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  ActivityTableViewCell.swift
+//  FinalChallengeApp
+//
+//  Created by Ni Wayan Bianka Aristania on 21/11/19.
+//  Copyright © 2019 George Joseph Kristian. All rights reserved.
+//
+
+import UIKit
+
+class ActivityTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var activityNameLabel: UILabel!
+    @IBOutlet weak var checkboxButton: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
