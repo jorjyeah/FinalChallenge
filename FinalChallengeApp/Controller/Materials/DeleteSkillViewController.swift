35daed509b21dcc8b0d154c7c6b3b1e626090383
Bianka <ni.aristania@binus.ac.id>
2019-12-06 13:46:45 +0700

    delete skill page

--- FinalChallengeApp/Controller/Materials/DeleteSkillViewController.swift
+++ FinalChallengeApp/Controller/Materials/DeleteSkillViewController.swift
@@ -1,14 +1,14 @@
 //
-//  NewSkillViewController.swift
+//  DeleteSkillViewController.swift
 //  FinalChallengeApp
 //
-//  Created by Ni Wayan Bianka Aristania on 05/12/19.
+//  Created by Ni Wayan Bianka Aristania on 06/12/19.
 //  Copyright © 2019 George Joseph Kristian. All rights reserved.
 //
 
 import UIKit
 
-class NewSkillViewController: UIViewController, UITextViewDelegate {
+class DeleteSkillViewController: UIViewController, UITextViewDelegate {
     
     @IBOutlet weak var backgroundImageView: UIImageView!
     
@@ -28,20 +28,19 @@ class NewSkillViewController: UIViewController, UITextViewDelegate {
         backgroundImageView.layer.cornerRadius = 8
         skillTitleTextView.textContainer.maximumNumberOfLines = 4
         skillTitleTextView.textContainer.lineBreakMode = .byClipping
-        
     }
     
     override func viewDidLayoutSubviews() {
-        skillTitleTextView.centerVertically()
-    }
-    
+            skillTitleTextView.centerVertically()
+        }
+        
     func textViewDidBeginEditing (_ textView: UITextView) {
         if skillTitleTextView.textColor == .black && skillTitleTextView.isFirstResponder {
             skillTitleTextView.text = nil
             skillTitleTextView.textColor = .black
         }
     }
-    
+        
     func textViewDidEndEditing (_ textView: UITextView) {
         if skillTitleTextView.text.isEmpty || skillTitleTextView.text == "" {
             skillTitleTextView.textColor = .black
@@ -51,13 +50,13 @@ class NewSkillViewController: UIViewController, UITextViewDelegate {
     }
 }
 
-extension UITextView {
-    func centerVertically() {
-        let fittingSize = CGSize(width: bounds.width, height: CGFloat.greatestFiniteMagnitude)
-        let size = sizeThatFits(fittingSize)
-        let topOffset = (bounds.size.height - size.height * zoomScale) / 2
-        let positiveTopOffset = max(1, topOffset)
-        contentOffset.y = -positiveTopOffset
-    }
 
-}
+//extension UITextView {
+//    func centerVertically() {
+//        let fittingSize = CGSize(width: bounds.width, height: CGFloat.greatestFiniteMagnitude)
+//        let size = sizeThatFits(fittingSize)
+//        let topOffset = (bounds.size.height - size.height * zoomScale) / 2
+//        let positiveTopOffset = max(1, topOffset)
+//        contentOffset.y = -positiveTopOffset
+//    }
+//}
